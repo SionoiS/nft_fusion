15188a88c96ab5459fc93c5d6a2d591a4c257d53
SionoiS <SionoiS@users.noreply.github.com>
2021-05-29 10:34:04 -0400

    Absratc contract

--- contracts/Alloy.sol
+++ contracts/Alloy.sol
@@ -1,12 +1,13 @@
-// contracts/GameItem.sol
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "../contracts/Composable.sol";
 
-contract Alloy is Ownable, ERC721URIStorage {
+contract Alloy is Ownable, ERC721URIStorage, Composable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -16,22 +17,18 @@ contract Alloy is Ownable, ERC721URIStorage {
     constructor() ERC721("Alloy", "ALY") {}
 
     /// Mint a standard token
-    function mintOre(string memory tokenURI)
-        public
-        onlyOwner
-        returns (uint256)
-    {
+    function mintOre(string memory URI) public onlyOwner returns (uint256) {
         _tokenIds.increment();
         uint256 newId = _tokenIds.current();
 
         _safeMint(msg.sender, newId);
-        _setTokenURI(newId, tokenURI);
+        _setTokenURI(newId, URI);
 
         return newId;
     }
 
     /// Mint a token redeemable only by a user who own the specified array of token Ids.
-    function mintAlloy(uint256[] memory ids, string memory tokenURI)
+    function mintAlloy(uint256[] memory ids, string memory URI)
         public
         onlyOwner
         returns (uint256)
@@ -40,39 +37,32 @@ contract Alloy is Ownable, ERC721URIStorage {
         uint256 newId = _tokenIds.current();
 
         _safeMint(msg.sender, newId);
-        _setTokenURI(newId, tokenURI);
-        _tokenParts[newId] = ids;
+        _setTokenURI(newId, URI);
+        _setRequirement(newId, ids);
 
         return newId;
     }
 
-    /// Fuse multiple tokens, burning them in the process and get a more exclusive token.
+    /// Fuse multiple ore tokens, burning them in the process and get an alloy token.
     function fuse(uint256 tokenId) public {
-        uint256[] storage parts = _tokenParts[tokenId];
-
-        uint256 i = parts.length;
-        for (i; i > 0; i--) {
-            uint256 id = parts[i];
-
-            require(ownerOf(id) == msg.sender); //Would this revert the burn in previous iteration???
-
-            _burn(id);
-        }
+        _compose(tokenId, msg.sender);
 
         safeTransferFrom(owner(), msg.sender, tokenId);
     }
 
-    /// Returns an array of token Ids needed to fuse this token.
-    function tokenRequirement(uint256 tokenId)
-        public
-        view
-        returns (uint256[] memory)
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
     {
-        return _tokenParts[tokenId];
+        ERC721URIStorage._burn(tokenId);
     }
 
-    /// Is this token an Alloy
-    function isAlloy(uint256 tokenId) public view returns (bool) {
-        return _tokenParts[tokenId].length != 0;
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return ERC721URIStorage.tokenURI(tokenId);
     }
 }
---
+++ contracts/Composable.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+abstract contract Composable is ERC721 {
+    // Mapping from token Id to array of ids.
+    mapping(uint256 => uint256[]) private _tokenParts;
+
+    function _setRequirement(uint256 tokenId, uint256[] memory ids)
+        internal
+        virtual
+    {
+        _tokenParts[tokenId] = ids;
+    }
+
+    /// Returns an array of token Ids needed to compose this token.
+    function getRequirements(uint256 tokenId)
+        public
+        view
+        virtual
+        returns (uint256[] memory)
+    {
+        return _tokenParts[tokenId];
+    }
+
+    /// Is this token a composition
+    function isComposition(uint256 tokenId) public view virtual returns (bool) {
+        return _tokenParts[tokenId].length != 0;
+    }
+
+    function _compose(uint256 tokenId, address user) internal virtual {
+        uint256[] storage parts = _tokenParts[tokenId];
+
+        uint256 i = parts.length;
+        for (i; i > 0; i--) {
+            uint256 id = parts[i];
+
+            require(ownerOf(id) == user); //Would this revert the burn in previous iteration???
+
+            _burn(id);
+        }
+    }
+}
