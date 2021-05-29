6cb3748a8b91415385fcf168268e143c3d39f30d
SionoiS <SionoiS@users.noreply.github.com>
2021-05-29 15:00:08 -0400

    Fix Metadata

--- contracts/Alloy.sol
+++ contracts/Alloy.sol
@@ -3,11 +3,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "../contracts/Composable.sol";
 
-contract Alloy is Ownable, Composable {
+contract Alloy is Ownable, Composable, ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -17,9 +17,19 @@ contract Alloy is Ownable, Composable {
     constructor() ERC721("Alloy", "ALY") {}
 
     /// Mint a standard token
-    function mintOre()
+    function mintOre(string memory URI) public onlyOwner returns (uint256) {
+        _tokenIds.increment();
+        uint256 newId = _tokenIds.current();
+
+        _safeMint(msg.sender, newId);
+        _setTokenURI(newId, URI);
+
+        return newId;
+    }
+
+    /// Mint a token redeemable only by a user who own the specified array of token Ids.
+    function mintAlloy(uint256[] memory ids, string memory URI)
         public
-        /* string memory URI */
         onlyOwner
         returns (uint256)
     {
@@ -27,20 +37,7 @@ contract Alloy is Ownable, Composable {
         uint256 newId = _tokenIds.current();
 
         _safeMint(msg.sender, newId);
-        //_setTokenURI(newId, URI);
-
-        return newId;
-    }
-
-    /// Mint a token redeemable only by a user who own the specified array of token Ids.
-    function mintAlloy(
-        uint256[] memory ids /* , string memory URI */
-    ) public onlyOwner returns (uint256) {
-        _tokenIds.increment();
-        uint256 newId = _tokenIds.current();
-
-        _safeMint(msg.sender, newId);
-        //_setTokenURI(newId, URI);
+        _setTokenURI(newId, URI);
         _setRequirements(newId, ids);
 
         return newId;
@@ -53,19 +50,19 @@ contract Alloy is Ownable, Composable {
         safeTransferFrom(owner(), msg.sender, tokenId);
     }
 
-    /* function _burn(uint256 tokenId)
+    function _burn(uint256 tokenId)
         internal
         override(ERC721, ERC721URIStorage)
     {
         ERC721URIStorage._burn(tokenId);
-    } */
+    }
 
-    /* function tokenURI(uint256 tokenId)
+    function tokenURI(uint256 tokenId)
         public
         view
         override(ERC721, ERC721URIStorage)
         returns (string memory)
     {
         return ERC721URIStorage.tokenURI(tokenId);
-    } */
+    }
 }
