0a9f98f5988fa7ea719549c13889c3f9d4c217c4
Eurvin Morman <eurvin@gmail.com>
2021-05-30 16:49:33 +0200

    added call to web3 browser client reading out the eth address displaying it in the browser

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import { useEffect, useState } from "react";
 
 function App() {
+
+    // useState Hook to declare ac
+   const [accounts,setAccounts] = useState();
+
+    useEffect(() => {
+        // load blockchain data, then set the accounts
+
+        loadBlockchain();
+    })
+
+
+    async function loadBlockchain() {
+        const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+        // const network = await web3.eth.net.getNetworkType();
+        const accounts = await web3.eth.getAccounts();
+        console.log(accounts)
+        await setAccounts(accounts);
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="">
+
       </header>
+      <main>
+        <div>
+          <h1>Hello World</h1>
+            <p>your account: {accounts}</p>
+        </div>
+      </main>
     </div>
   );
 }
